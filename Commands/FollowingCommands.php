287360b18a1a568301b0f1ac9e1ab4a83961455d
jaroslavtyc <jaroslalvtyc@atlas.cz>
2013-08-29 22:15:48 +0200

    Removed commands list dependency, added support for new commands

--- Commands/FollowingCommands.php
+++ Commands/FollowingCommands.php
@@ -1,92 +1,166 @@
 <?php
 namespace Pribi\Commands;
 
-/**
- * @method as ($alias) @return Command
- */
 class FollowingCommands extends \Pribi\Core\Object {
-	use Alias;
 
-	private $commands;
-	private $lastCommandWithIdentificator;
+	private $lastCommandBringingIdentificator;
 	private $lastCommand;
-
-	protected function __construct(Commands $commands) {
-		$this->commands = $commands;
-	}
 
 	public function select($identificator) {
 		$select = new Select($identificator, $this);
-		$this->setLastCommandWithIdentificator($select);
+		$this->setLastCommandBringingIdentificator($select);
 
 		return $select;
 	}
 
 	public function from($identificator) {
 		$from = new From($identificator, $this);
-		$this->setLastCommandWithIdentificator($from);
+		$this->setLastCommandBringingIdentificator($from);
 
 		return $from;
 	}
 
 	public function innerJoin($identificator) {
 		$innerJoin = new InnerJoin($identificator, $this);
-		$this->setLastCommandWithIdentificator($innerJoin);
+		$this->setLastCommandBringingIdentificator($innerJoin);
 
 		return $innerJoin;
 	}
 
 	public function leftJoin($identificator) {
 		$leftJoin = new LeftJoin($identificator, $this);
-		$this->setLastCommandWithIdentificator($leftJoin);
+		$this->setLastCommandBringingIdentificator($leftJoin);
 
 		return $leftJoin;
 	}
 
 	public function rightJoin($identificator) {
 		$rightJoin = new RightJoin($identificator, $this);
-		$this->setLastCommandWithIdentificator($rightJoin);
+		$this->setLastCommandBringingIdentificator($rightJoin);
 
 		return $rightJoin;
 	}
 
 	public function on($identificator) {
 		$on = new On($identificator, $this);
-		$this->setLastCommandWithIdentificator($on);
+		$this->setLastCommand($on);
 
 		return $on;
 	}
 
+	public function conjunction($identificator) {
+		$conjunction = new Conjunction($identificator, $this);
+		$this->setLastCommand($conjunction);
+
+		return $conjunction;
+	}
+
+	public function disjunction($identificator) {
+		$disjunction = new Disjunction($identificator, $this);
+		$this->setLastCommand($disjunction);
+
+		return $disjunction;
+	}
+
+	public function negation() {
+		$negation = new Negation($this);
+		$this->setLastCommand($negation);
+
+		return $negation;
+	}
+
+	public function in($firstIdentificator) {
+		$inArray = new InArray(\func_get_args(), $this);
+		$this->setLastCommand($inArray);
+
+		return $inArray;
+	}
+
+	public function inArray(array $identificators) {
+		$inArray = new InArray($identificators, $this);
+		$this->setLastCommand($inArray);
+
+		return $inArray;
+	}
+
+	public function equalTo($identificator) {
+		$equalTo = new EqualTo($identificator, $this);
+		$this->setLastCommand($equalTo);
+
+		return $equalTo;
+	}
+
+	public function equalOrGreaterThen($identificator) {
+		$equalOrGreaterThen = new EqualOrGreaterThen($identificator, $this);
+		$this->setLastCommand($equalOrGreaterThen);
+
+		return $equalOrGreaterThen;
+	}
+
+	public function greaterThen($identificator) {
+		$greaterThen = new GreaterThen($identificator, $this);
+		$this->setLastCommand($greaterThen);
+
+		return $greaterThen;
+	}
+
+	public function equalOrLesserThen($identificator) {
+		$equalOrLesserThen = new EqualOrLesserThen($identificator, $this);
+		$this->setLastCommand($equalOrLesserThen);
+
+		return $equalOrLesserThen;
+	}
+
+	public function lesserThen($identificator) {
+		$lesserThen = new LesserThen($identificator, $this);
+		$this->setLastCommand($lesserThen);
+
+		return $lesserThen;
+	}
+
+	public function differentTo($identificator) {
+		$differentTo = new DifferentTo($identificator, $this);
+		$this->setLastCommand($differentTo);
+
+		return $differentTo;
+	}
+
 	public function where($identificator) {
 		$where = new Where($identificator, $this);
-		$this->setLastCommandWithIdentificator($where);
+		$this->setLastCommand($where);
 
 		return $where;
 	}
 
-	public function limit($limit) {
-		return new Limit(0, $limit);
+	public function limit($amount) {
+		$limit = new Limit(0, $amount);
+		$this->setLastCommand($limit);
+
+		return $limit;
 	}
 
-	public function offsetAndLimit($offset, $limit) {
-		return new Limit($offset, $limit);
+	public function offsetAndLimit($offset, $amount) {
+		$amount = new Limit($offset, $amount);
+		$this->setLastCommand($amount);
+
+		return $amount;
 	}
 
 	/**
 	 * @return CommandBringingIdentificator
 	 */
 	protected function alias($name) {
-		if ($this->getLastCommandWithIdentificator() === $this->getLastCommand()) {
-			$this->getLastCommandWithIdentificator()->setAlias($name);
+		if ($this->getLastCommandBringingIdentificator() === $this->getLastCommand()) {
+			$this->getLastCommandBringingIdentificator()->setAlias($name);
 		} else {
 			throw new Exceptions\AliasHasToDirectlyFollowIdentificator(sprintf('Followed [%s]', \get_class($this->getLastCommand())));
 		}
 
-		return $this->getLastCommandWithIdentificator();
+		return $this->getLastCommandBringingIdentificator();
 	}
 
-	private function setLastCommandWithIdentificator(CommandBringingIdentificator $command) {
-		$this->lastCommandWithIdentificator = $command;
+	private function setLastCommandBringingIdentificator(CommandBringingIdentificator $command) {
+		$this->lastCommandBringingIdentificator = $command;
 		$this->setLastCommand($command);
 	}
 
@@ -97,8 +171,8 @@ class FollowingCommands extends \Pribi\Core\Object {
 	/**
 	 * @return CommandBringingIdentificator
 	 */
-	private function getLastCommandWithIdentificator() {
-		return $this->lastCommandWithIdentificator;
+	private function getLastCommandBringingIdentificator() {
+		return $this->lastCommandBringingIdentificator;
 	}
 
 	/**
@@ -106,12 +180,5 @@ class FollowingCommands extends \Pribi\Core\Object {
 	 */
 	private function getLastCommand() {
 		return $this->lastCommand;
-	}
-
-	/**
-	 * @return Commands
-	 */
-	protected function getCommands() {
-		return $this->commands;
 	}
 }
