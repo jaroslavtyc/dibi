acfd40b8390ebc3f14ae01ad5fabff9d0fd6d663
jaroslavtyc <jaroslalvtyc@atlas.cz>
2013-09-23 18:58:03 +0200

    Shortcut for chain of commands initiation

---
+++ Commands/First.php
@@ -0,0 +1,34 @@
+<?php
+namespace Pribi\Commands;
+
+use Pribi\Commands\Selects\Select;
+use Pribi\Commands\Transactions\Begins\Begin;
+use Pribi\Commands\Transactions\BeginWorks\BeginWork;
+use Pribi\Commands\Transactions\StartTransactions\StartTransaction;
+use Pribi\Core\Object;
+
+class First extends Object implements Command {
+	public function insert() {
+		return new Insert();
+	}
+
+	public function select($subject) {
+		return new Select($subject);
+	}
+
+	public function delete($subject = FALSE) {
+		return new Delete($subject);
+	}
+
+	public function begin() {
+		return new Begin();
+	}
+
+	public function beginWork() {
+		return new BeginWork();
+	}
+
+	public function startTransaction() {
+		return new StartTransaction();
+	}
+}
