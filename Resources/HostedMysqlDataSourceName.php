f914244ee2c202d2a8685ebc5bf52c236028c8c4
jaroslavtyc <jaroslalvtyc@atlas.cz>
2013-10-27 12:19:30 +0100

    bugfix: Hosted MySQL DSN requires proper parameters

--- Resources/HostedMysqlDataSourceName.php
+++ Resources/HostedMysqlDataSourceName.php
@@ -2,7 +2,7 @@
 namespace Pribi\Resources;
 
 class HostedMysqlDataSourceName extends DataSourceName {
-	public function __construct($host, $port, $socket, $databaseName = FALSE) {
+	public function __construct($host, $port, $databaseName = FALSE) {
 		parent::__construct($host, $port, FALSE, $databaseName);
 	}
 }
