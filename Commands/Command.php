27a0f82bd3e614f522abbfd863ba76ee2eac6663
tyc <jaroslavtyc@atlas.cz>
2013-11-07 16:15:29 +0100

    Result is freed from memory at end of life-cycle and can not be rewind multiple times

--- Commands/Command.php
+++ Commands/Command.php
@@ -1,6 +1,25 @@
 <?php
 namespace Pribi\Commands;
 
-interface Command {
+use Pribi\Core\Object;
 
+abstract class Command extends Object {
+	private $previousCommand;
+
+	public function __construct(Command $previousCommand) {
+		$this->previousCommand = $previousCommand;
+	}
+
+	abstract protected function toSql();
+
+	protected function hasPreviousCommand() {
+		return isset($this->previousCommand);
+	}
+
+	/**
+	 * @return Command
+	 */
+	protected function getPreviousCommand() {
+		return $this->previousCommand;
+	}
 }
