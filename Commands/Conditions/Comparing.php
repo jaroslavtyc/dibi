20c8e0e5fe8f0b72b16e2b7321278c0ecac09707
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-02-12 17:48:21 +0100

    Comparing trait introduced.

---
+++ Commands/Conditions/Comparing.php
@@ -0,0 +1,48 @@
+<?php
+namespace Pribi\Commands\Conditions;
+
+use Pribi\Commands\Identifiers\Identifier;
+
+trait Comparing {
+	public function equalTo($subject) {
+		/**
+		 * @var \Pribi\Commands\Command $this
+		 */
+		return new EqualTo(new Identifier($subject), $this);
+	}
+	
+	public function equalOrGreaterThen($subject) {
+		/**
+		 * @var \Pribi\Commands\Command $this
+		 */
+		return new EqualOrGreaterThen(new Identifier($subject), $this);
+	}
+
+	public function equalOrLesserThen($subject) {
+		/**
+		 * @var \Pribi\Commands\Command $this
+		 */
+		return new EqualOrLesserThen(new Identifier($subject), $this);
+	}
+
+	public function greaterThen($subject) {
+		/**
+		 * @var \Pribi\Commands\Command $this
+		 */
+		return new GreaterThen(new Identifier($subject), $this);
+	}
+
+	public function lesserThen($subject) {
+		/**
+		 * @var \Pribi\Commands\Command $this
+		 */
+		return new LesserThen(new Identifier($subject), $this);
+	}
+
+	public function differentTo($subject) {
+		/**
+		 * @var \Pribi\Commands\Command $this
+		 */
+		return new DifferentTo(new Identifier($subject), $this);
+	}
+}
