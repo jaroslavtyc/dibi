e09e1f7c7aca397a523ea60687d7950bb0caba0d
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-02-17 12:47:36 +0100

    Null comparison can be used.

--- Commands/Conditions/IsNotNull.php
+++ Commands/Conditions/IsNotNull.php
@@ -4,5 +4,9 @@ namespace Pribi\Commands\Conditions;
 use Pribi\Commands\WithoutIdentifier;
 
 class IsNotNull extends WithoutIdentifier {
+use AndOring;
 
+protected function toSql() {
+	return 'IS NOT NULL';
+}
 }
--- Commands/Conditions/IsNull.php
+++ Commands/Conditions/IsNull.php
@@ -4,5 +4,9 @@ namespace Pribi\Commands\Conditions;
 use Pribi\Commands\WithoutIdentifier;
 
 class IsNull extends WithoutIdentifier {
+	use AndOring;
 
+	protected function toSql() {
+		return 'IS NULL';
+	}
 }
