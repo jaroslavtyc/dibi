33050c3fd05b607e16b4bd5cc4318b833faace8e
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-02-19 17:41:28 +0100

    Base join command fixed.

--- Commands/Joins/Join.php
+++ Commands/Joins/Join.php
@@ -1,14 +1,14 @@
 <?php
 namespace Pribi\Commands\Joins;
 
+use Pribi\Commands\Identifiers\Identifier;
 use Pribi\Commands\Identifiers\IdentifierBringer;
-use Pribi\Commands\Joins\Inner\InnerJoinAlias;
 
 /**
  * @method InnerJoinAlias as ($alias)
  */
 abstract class Join extends IdentifierBringer {
-	public function on($identificator) {
-		return new On($identificator, $this);
+	public function on($subject) {
+		return new On(new Identifier($subject), $this);
 	}
 }
