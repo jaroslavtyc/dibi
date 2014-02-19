0f96771cd784c150745e8859153b6a8e0277eac8
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-02-19 17:41:07 +0100

    Join and-or-ing trait returns objects from proper namespaces.

--- Commands/Joins/AndOring.php
+++ Commands/Joins/AndOring.php
@@ -1,10 +1,44 @@
 <?php
 namespace Pribi\Commands\Joins;
 
+use Pribi\Commands\Identifiers\Identifier;
+
 /**
  * @method Conjunction and($subject)
  * @method Disjunction or($subject)
  */
 trait AndOring {
 	use \Pribi\Commands\Conditions\AndOring;
+
+	protected function conjunction(Identifier $identifier) {
+		/**
+		 * @var \Pribi\Commands\Command $this
+		 */
+		$conjunction = new Conjunction($identifier, $this);
+
+		return $conjunction;
+	}
+
+	protected function disjunction(Identifier $identifier) {
+		/**
+		 * @var \Pribi\Commands\Command $this
+		 */
+		$disjunction = new Disjunction($identifier, $this);
+
+		return $disjunction;
+	}
+
+	public function andNot($subject) {
+		/**
+		 * @var \Pribi\Commands\Command $this
+		 */
+		return new AndNot(new Identifier($subject), $this);
+	}
+
+	public function orNot($subject) {
+		/**
+		 * @var \Pribi\Commands\Command $this
+		 */
+		return new OrNot(new Identifier($subject), $this);
+	}
 }
