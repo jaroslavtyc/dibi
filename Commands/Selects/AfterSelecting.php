2750115490c4a64fd0c0013c5200f140cc9a9994
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-02-25 17:42:58 +0100

    FIxed trait with commands after select.

--- Commands/Selects/AfterSelecting.php
+++ Commands/Selects/AfterSelecting.php
@@ -4,49 +4,50 @@ namespace Pribi\Commands\Selects;
 use Pribi\Commands\Conditions\Where;
 use Pribi\Commands\Conditions\WhereNot;
 use Pribi\Commands\FromSources\From;
+use Pribi\Commands\Identifiers\Identifier;
 
 trait AfterSelecting {
-	public function select($identificator) {
+	public function select($subject) {
 		/**
 		 * @var \Pribi\Commands\Command $this
 		 */
-		$select = new Select($identificator, $this);
+		$select = new Select(new Identifier($subject), $this);
 
 		return $select;
 	}
 
-	public function selectNot($identificator) {
+	public function selectNot($subject) {
 		/**
 		 * @var \Pribi\Commands\Command $this
 		 */
-		$select = new SelectNot($identificator, $this);
+		$select = new SelectNot(new Identifier($subject), $this);
 
 		return $select;
 	}
 
-	public function from($identificator) {
+	public function from($subject) {
 		/**
 		 * @var \Pribi\Commands\Command $this
 		 */
-		$from = new From($identificator, $this);
+		$from = new From(new Identifier($subject), $this);
 
 		return $from;
 	}
 
-	public function where($identificator) {
+	public function where($subject) {
 		/**
 		 * @var \Pribi\Commands\Command $this
 		 */
-		$where = new Where($identificator, $this);
+		$where = new Where(new Identifier($subject), $this);
 
 		return $where;
 	}
 
-	public function whereNot($identificator) {
+	public function whereNot($subject) {
 		/**
 		 * @var \Pribi\Commands\Command $this
 		 */
-		$where = new WhereNot($identificator, $this);
+		$where = new WhereNot(new Identifier($subject), $this);
 
 		return $where;
 	}
