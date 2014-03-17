d8aac9e09013a1f0f58f11eb59451fb797f4b697
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-03-17 14:16:03 +0100

    Join equal to condition updated to reflect component names changes.

--- Commands/Joins/Equalto.php
+++ Commands/Joins/Equalto.php
@@ -2,8 +2,10 @@
 namespace Pribi\Commands\Joins;
 
 use Pribi\Commands\Conditions\Base\AndOrUsable;
-use Pribi\Commands\Limits\Limitable;
-use Pribi\Commands\Limits\Limiting;
+use Pribi\Commands\Joins\Base\AndOring;
+use Pribi\Commands\Joins\Base\Joinable;
+use Pribi\Commands\Limits\Base\Limitable;
+use Pribi\Commands\Limits\Base\Limiting;
 
 class EqualTo extends \Pribi\Commands\Conditions\EqualTo implements AndOrUsable, Joinable, Limitable {
 	use AndOring;
