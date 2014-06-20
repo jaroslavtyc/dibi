32ea964211f61417c4fb40201d72aa5d832cb218
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-06-20 09:43:48 +0200

    Tests bootstrap autoloader ignores unknown classes.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,9 @@
  * Simple autoloader using the PCR-0 Pribi naming convention, @see http://www.php-fig.org/psr/psr-0/
  * For example Pribi\Commands\Inserts/Insert is searched in "__DIR__ . '/../Pribi/Commands/Inserts/Insert.php" file.
  */
+
 spl_autoload_register(function ($className) {
-	include __DIR__ . '/../' . $className . '.php';
+	if (file_exists(__DIR__ . '/../' . $className . '.php')) {
+		require __DIR__ . '/../' . $className . '.php';
+	}
 });
