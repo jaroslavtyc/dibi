6d67859f878c1c2feb110f20a721f1f15e8fd788
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-07-07 17:18:29 +0200

    Without identificator class existence commented.

--- Pribi/Commands/WithoutIdentifier.php
+++ Pribi/Commands/WithoutIdentifier.php
@@ -1,6 +1,12 @@
 <?php
 namespace Pribi\Commands;
 
+/**
+ * Class WithoutIdentifier
+ * @package Pribi\Commands
+ *
+ * Well, nothing to see here at first, but notice the class name - it is exactly what everyone will search for after use of WithIdentifier class.
+ */
 abstract class WithoutIdentifier extends Command {
 
 }
