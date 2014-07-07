1dc4e2dc1a27db7eafac202df32eecb5a2bc8fc6
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-07-07 14:41:36 +0200

    Pribi helper test has renamed test methods to increase readability.

--- tests/Pribi/PribiTest.php
+++ tests/Pribi/PribiTest.php
@@ -1,12 +1,39 @@
 <?php
 namespace Pribi;
 
-class PribiTest extends PHPUnit_Framework_TestCase {
-	public function testOpenQueryPublicStaticFunctionExists() {
-		$this->assertTrue(method_exists(Pribi::class, 'openQuery'));
+use Pribi\Commands\QueryOpener;
+use Pribi\Commands\Subconditions\Subcondition;
+
+class PribiTest extends \PHPUnit_Framework_TestCase {
+	public function testOpenQueryAsPublicStaticFunctionExists() {
+		$reflection = new \ReflectionClass(Pribi::class);
+		$this->assertTrue($reflection->hasMethod('openQuery'));
+		$methodReflection = $reflection->getMethod('openQuery');
+		$this->assertTrue($methodReflection->isPublic());
+		$this->assertTrue($methodReflection->isStatic());
 	}
 
-	public function testSubconditionPublicStaticFunctionExists() {
-		$this->assertTrue(method_exists(Pribi::class, 'openQuery'));
+	public function testCanGiveQueryOpener() {
+		$this->assertEquals(new QueryOpener(), Pribi::openQuery());
+	}
+
+	public function testEveryGivenQueryOpenerIsANewInstance() {
+		$this->assertNotSame(Pribi::openQuery(), Pribi::openQuery());
+	}
+
+	public function testSubconditionAsPublicStaticFunctionExists() {
+		$reflection = new \ReflectionClass(Pribi::class);
+		$this->assertTrue($reflection->hasMethod('subcondition'));
+		$methodReflection = $reflection->getMethod('subcondition');
+		$this->assertTrue($methodReflection->isPublic());
+		$this->assertTrue($methodReflection->isStatic());
+	}
+
+	public function testCanGiveSubcondition() {
+		$this->assertEquals(new Subcondition(), Pribi::subcondition());
+	}
+
+	public function testEveryGivenSubconditionIsANewInstance() {
+		$this->assertNotSame(Pribi::subcondition(), Pribi::subcondition());
 	}
 }
