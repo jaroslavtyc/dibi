4208f4f16aa1c4b78ca1520b56eef15ddb9c3351
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-07-07 14:36:12 +0200

    Test bootstrap can load even vendor files.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,10 @@
  * For example Pribi\Commands\Inserts/Insert is searched in "__DIR__ . '/../Pribi/Commands/Inserts/Insert.php" file.
  */
 
+require __DIR__ . '/../vendor/autoload.php';
+
 spl_autoload_register(function ($className) {
-	if (file_exists(__DIR__ . '/../' . $className . '.php')) {
-		require __DIR__ . '/../' . $className . '.php';
+	if (file_exists(__DIR__ . '/../' . str_replace('\\', '/', $className) . '.php')) {
+		require __DIR__ . '/../' . str_replace('\\', '/', $className) . '.php';
 	}
 });
