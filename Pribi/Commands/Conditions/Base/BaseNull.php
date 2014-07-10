36558bf211206d57dafe1aa55c3d4d13f372b681
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-07-10 09:15:58 +0200

    Base null fixed.

--- Pribi/Commands/Conditions/Base/BaseNull.php
+++ Pribi/Commands/Conditions/Base/BaseNull.php
@@ -1,7 +1,7 @@
 <?php
 namespace Pribi\Commands\Conditions\Base;
 
-use Pribi\Commands\Conditions\WithoutIdentifier;
+use Pribi\Commands\WithoutIdentifier;
 
 abstract class Null extends WithoutIdentifier implements AndOrUsable {
 	use AndOring;
