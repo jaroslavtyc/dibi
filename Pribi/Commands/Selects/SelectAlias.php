71a2c9324683310388ddc31efb2e09a90bd12bc5
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-07-10 09:15:27 +0200

    Alias of select and select not altered to use command builder.

--- Pribi/Commands/Selects/SelectAlias.php
+++ Pribi/Commands/Selects/SelectAlias.php
@@ -5,6 +5,13 @@ use Pribi\Builders\CommandsBuilder;
 use Pribi\Commands\Identifiers\Identifier;
 
 class SelectAlias extends Base\SelectAlias {
+	/**
+	 * Constructor with closely specified PreviousCommand, respectively aliased Select
+	 *
+	 * @param Identifier $alias
+	 * @param Select $prependSelect
+	 * @param CommandsBuilder $commandsBuilder
+	 */
 	public function __construct(Identifier $alias, Select $prependSelect, CommandsBuilder $commandsBuilder) {
 		parent::__construct($alias, $prependSelect, $commandsBuilder);
 	}
--- Pribi/Commands/Selects/SelectNotAlias.php
+++ Pribi/Commands/Selects/SelectNotAlias.php
@@ -1,10 +1,18 @@
 <?php
 namespace Pribi\Commands\Selects;
 
+use Pribi\Builders\CommandsBuilder;
 use Pribi\Commands\Identifiers\Identifier;
 
 class SelectNotAlias extends Base\SelectAlias {
-	public function __construct(Identifier $alias, SelectNot $prependSelectNot) {
-		parent::__construct($alias, $prependSelectNot);
+	/**
+	 * Constructor with closely specified PreviousCommand, respectively aliased SelectNot
+	 *
+	 * @param Identifier $alias
+	 * @param SelectNot $prependSelectNot
+	 * @param CommandsBuilder $commandsBuilder
+	 */
+	public function __construct(Identifier $alias, SelectNot $prependSelectNot, CommandsBuilder $commandsBuilder) {
+		parent::__construct($alias, $prependSelectNot, $commandsBuilder);
 	}
 }
