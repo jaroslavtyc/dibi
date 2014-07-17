9db99f3cdef9c18ae33ed69c6d7e22aab685ee72
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-07-17 17:57:36 +0200

    TestCase class is loaded by test bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,8 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+require_once __DIR__ . '/helpers/TestCase.php';
+
 spl_autoload_register(function ($className) {
 	if (file_exists(__DIR__ . '/../' . str_replace('\\', '/', $className) . '.php')) {
 		require __DIR__ . '/../' . str_replace('\\', '/', $className) . '.php';
