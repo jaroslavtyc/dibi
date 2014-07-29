e68821c16d0fb524b93894f3f42f2728c4bba3fb
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-07-29 23:07:38 +0200

    refactoring: Referenced class names changed to fully classified name.

--- Pribi/Commands/WithIdentifier.php
+++ Pribi/Commands/WithIdentifier.php
@@ -1,17 +1,17 @@
 <?php
 namespace Pribi\Commands;
 
-use Pribi\Builders\CommandsBuilder;
-use Pribi\Commands\Identifiers\Identifier;
-
 abstract class WithIdentifier extends Command {
 	private $identifier;
 
-	public function __construct(Identifier $identifier, Command $previousCommand, CommandsBuilder $commandsBuilder) {
+	public function __construct(\Pribi\Commands\Identifiers\Identifier $identifier, Command $previousCommand, \Pribi\Builders\CommandsBuilder $commandsBuilder) {
 		$this->identifier = $identifier;
 		parent::__construct($previousCommand, $commandsBuilder);
 	}
 
+	/**
+	 * @return \Pribi\Commands\Identifiers\Identifier
+	 */
 	protected function getIdentifier() {
 		return $this->identifier;
 	}
