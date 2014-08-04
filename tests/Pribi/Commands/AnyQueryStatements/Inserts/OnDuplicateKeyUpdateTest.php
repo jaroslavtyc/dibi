0386d0d73dc46788c9607a4e3c270c176f65def9
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-08-04 11:37:03 +0200

    ON DUPLICATE KEY UPDATE covered by tests.

---
+++ tests/Pribi/Commands/AnyQueryStatements/Inserts/OnDuplicateKeyUpdateTest.php
@@ -0,0 +1,64 @@
+<?php
+namespace Pribi\Commands\AnyQueryStatements\Inserts;
+
+class OnDuplicateKeyUpdateTest extends \Tests\Helpers\CommandTestCase {
+
+	public function testCanCreateInstance() {
+		$instance = new OnDuplicateKeyUpdate($this->createIdentifierDummy(), $this->createSubjectDummy(), $this->createCommandDummy(), $this->getCommandsBuilderDummy());
+		$this->assertNotNull($instance);
+	}
+
+	public function testAsSqlIsOnDuplicateKeyUpdateKeywordFollowedByIdentifierEqualToExpression() {
+		$toSqlMethod = $this->createToSqlReflectionMethod(OnDuplicateKeyUpdate::class);
+		$identifierMock = $this->getMockBuilder(\Pribi\Commands\Identifiers\Identifier::class)
+			->disableOriginalConstructor()
+			->setMethods(['toSql'])
+			->getMock();
+		$identifierMock->expects($this->once())
+			->method('toSql')
+			->willReturn($identifierAsSql = 'foo');
+		$expressionMock = $this->getMockBuilder(\Pribi\Commands\Subjects\Subject::class)
+			->disableOriginalConstructor()
+			->setMethods(['toSql'])
+			->getMock();
+		$expressionMock->expects($this->once())
+			->method('toSql')
+			->willReturn($expressionAsSql = 'bar');
+		/**
+		 * @var \Pribi\Commands\Identifiers\Identifier $identifierMock
+		 * @var \Pribi\Commands\Subjects\Subject $expressionMock
+		 */
+		$onDuplicateKeyUpdate = new OnDuplicateKeyUpdate($identifierMock, $expressionMock, $this->createCommandDummy(), $this->getCommandsBuilderDummy());
+		$this->assertSame("ON DUPLICATE KEY UPDATE $identifierAsSql=$expressionAsSql", $toSqlMethod->invoke($onDuplicateKeyUpdate));
+	}
+
+	public function testAsSqlIfChainedIsCommaFollowedByIdentifierEqualToExpression() {
+		$toSqlMethod = $this->createToSqlReflectionMethod(OnDuplicateKeyUpdate::class);
+		$identifierMock = $this->getMockBuilder(\Pribi\Commands\Identifiers\Identifier::class)
+			->disableOriginalConstructor()
+			->setMethods(['toSql'])
+			->getMock();
+		$identifierMock->expects($this->once())
+			->method('toSql')
+			->willReturn($identifierAsSql = 'foo');
+		$expressionMock = $this->getMockBuilder(\Pribi\Commands\Subjects\Subject::class)
+			->disableOriginalConstructor()
+			->setMethods(['toSql'])
+			->getMock();
+		$expressionMock->expects($this->once())
+			->method('toSql')
+			->willReturn($expressionAsSql = 'bar');
+		/**
+		 * @var \Pribi\Commands\Identifiers\Identifier $identifierMock
+		 * @var \Pribi\Commands\Subjects\Subject $expressionMock
+		 */
+		$onDuplicateKeyUpdate = new OnDuplicateKeyUpdate(
+			$identifierMock,
+			$expressionMock,
+			$this->getMockBuilder(OnDuplicateKeyUpdate::class)->disableOriginalConstructor()->getMock(),
+			$this->getCommandsBuilderDummy()
+		);
+		$this->assertSame(",$identifierAsSql=$expressionAsSql", $toSqlMethod->invoke($onDuplicateKeyUpdate));
+	}
+}
+ 
