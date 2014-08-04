959f3341a39bbfedc3e1c12b869876460fd6e78b
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-08-04 11:00:07 +0200

    Fixed typing error at inner method name.

--- tests/Pribi/Commands/Identifiers/IdentifierTest.php
+++ tests/Pribi/Commands/Identifiers/IdentifierTest.php
@@ -18,61 +18,61 @@ class IdentifierTest extends \PHPUnit_Framework_TestCase {
 	public function testWithEmptyStringAsSubjectIsEmptyStringAsSql() {
 		$toSqlMethod = new \ReflectionMethod(Identifier::class, 'toSql');
 		$toSqlMethod->setAccessible(TRUE);
-		$this->assertSame('', $this->getASIdentifierTurnedToSql(''));
+		$this->assertSame('', $this->getAsIdentifierTurnedToSql(''));
 	}
 
-	private function getASIdentifierTurnedToSql($subject) {
+	private function getAsIdentifierTurnedToSql($subject) {
 		return $this->accessibleIdentifierToSqlMethod->invoke(new Identifier($subject));
 	}
 
 	public function testWithPositiveIntegerAsSubjectIsSameDigitsAsSql() {
 		$number = 123456789;
-		$this->assertSame($number, $this->getASIdentifierTurnedToSql($number));
+		$this->assertSame($number, $this->getAsIdentifierTurnedToSql($number));
 	}
 
 	public function testWithNegativeIntegerAsSubjectIsSameDigitsAsSql() {
 		$number = -987654321;
-		$this->assertSame($number, $this->getASIdentifierTurnedToSql($number));
+		$this->assertSame($number, $this->getAsIdentifierTurnedToSql($number));
 	}
 
 	public function testWithPositiveFloatAsSubjectIsSameFloatAsSql() {
 		$number = 123456.789;
-		$this->assertSame($number, $this->getASIdentifierTurnedToSql($number));
+		$this->assertSame($number, $this->getAsIdentifierTurnedToSql($number));
 	}
 
 	public function testWithNegativeFloatAsSubjectIsSameFloatAsSql() {
 		$number = -987654.321;
-		$this->assertSame($number, $this->getASIdentifierTurnedToSql($number));
+		$this->assertSame($number, $this->getAsIdentifierTurnedToSql($number));
 	}
 
 	public function testWordIsQuotedAsSql() {
 		$word = 'foo';
-		$this->assertSame("`$word`", $this->getASIdentifierTurnedToSql($word));
+		$this->assertSame("`$word`", $this->getAsIdentifierTurnedToSql($word));
 	}
 
 	public function testWordsWithSpacesDelimiterAreQuotedAsSingleIdentifierAsSql() {
 		$moreWords = 'foo bar';
-		$this->assertSame("`$moreWords`", $this->getASIdentifierTurnedToSql($moreWords));
+		$this->assertSame("`$moreWords`", $this->getAsIdentifierTurnedToSql($moreWords));
 	}
 
 	public function testDigitsWithEvenSingleCharacterAreQuotedAsSql() {
 		$almostNumber = '987654321f';
-		$this->assertSame("`$almostNumber`", $this->getASIdentifierTurnedToSql($almostNumber));
+		$this->assertSame("`$almostNumber`", $this->getAsIdentifierTurnedToSql($almostNumber));
 	}
 
 	public function testNullValueIsUnquotedTextWithNullWordAsSql() {
-		$this->assertSame('NULL', $this->getASIdentifierTurnedToSql(NULL));
+		$this->assertSame('NULL', $this->getAsIdentifierTurnedToSql(NULL));
 	}
 
 	public function testStarAsWildcardCharacterIsNotQuotedAsSql() {
-		$this->assertSame('*', $this->getASIdentifierTurnedToSql('*'));
+		$this->assertSame('*', $this->getAsIdentifierTurnedToSql('*'));
 	}
 
 	public function testIdentifierFromMoreQuantifiersHasEachOfThemQuotedAsSql() {
 		$firstQualifier = 'foo';
 		$lastQualifier = 'bar';
 		$subject = "$firstQualifier.$lastQualifier"; // as table_name.ColumnName for example
-		$this->assertSame("`$firstQualifier`.`$lastQualifier`", $this->getASIdentifierTurnedToSql($subject));
+		$this->assertSame("`$firstQualifier`.`$lastQualifier`", $this->getAsIdentifierTurnedToSql($subject));
 	}
 
 	public function testStarAsWildcardAsOneOfQuantifiersOfIdentifierIsNotQuotedAsSql() {
@@ -80,16 +80,16 @@ class IdentifierTest extends \PHPUnit_Framework_TestCase {
 		$middleQuantifier = 'bar';
 		$lastQuantifier = '*';
 		$subject = "$firstQuantifier.$middleQuantifier.$lastQuantifier"; // as database_name.table_name.* for example
-		$this->assertSame("`$firstQuantifier`.`$middleQuantifier`.$lastQuantifier", $this->getASIdentifierTurnedToSql($subject));
+		$this->assertSame("`$firstQuantifier`.`$middleQuantifier`.$lastQuantifier", $this->getAsIdentifierTurnedToSql($subject));
 	}
 
 	public function testSqlFunctionNameRecognizedByBracketIsNotQuotedAsSql() {
 		$function = 'FOO(bar)';
-		$this->assertSame($function, $this->getASIdentifierTurnedToSql($function));
+		$this->assertSame($function, $this->getAsIdentifierTurnedToSql($function));
 	}
 
 	public function testAlreadyQuotedSubjectIsNotQuotedAgainAsSql() {
 		$subject = '`foo`.`bar`.`baz`';
-		$this->assertSame($subject, $this->getASIdentifierTurnedToSql($subject));
+		$this->assertSame($subject, $this->getAsIdentifierTurnedToSql($subject));
 	}
 }
