ebad70dc62a1cd24a326228695100aabf8b5e438
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-08-04 10:58:19 +0200

    HIGH PRIORITY can be followed by INTO.

---
+++ tests/Pribi/Commands/AnyQueryStatements/Inserts/HighPriorityTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace Pribi\Commands\AnyQueryStatements\Inserts;
+
+class HighPriorityTest extends \Tests\Helpers\CommandTestCase {
+
+	public function testCanCreateInstance() {
+		$highPriority = new HighPriority($this->createCommandDummy(), $this->getCommandsBuilderDummy());
+		$this->assertNotNull($highPriority);
+	}
+
+	public function testAsSqlIsHighPriorityKeyword() {
+		$toSqlMethod = new \ReflectionMethod(HighPriority::class, 'toSql');
+		$toSqlMethod->setAccessible(TRUE);
+		$highPriority = $this->createHighPriority($this->getCommandsBuilderDummy());
+		$this->assertSame('HIGH PRIORITY', $toSqlMethod->invoke($highPriority));
+	}
+
+	public function testCanUseIgnore() {
+		$commandsBuilderMock = $this->getMock(\Pribi\Builders\CommandsBuilder::class);
+		/** @var \Pribi\Builders\CommandsBuilder $commandsBuilderMock */
+		$highPriority = $this->createHighPriority($commandsBuilderMock);
+		/** @var \PHPUnit_Framework_MockObject_MockObject $commandsBuilderMock */
+		$commandsBuilderMock
+			->expects($this->once())
+			->method('createIgnore')
+			->with($highPriority)
+			->willReturn($ignoreDummy = 'foo');
+		$this->assertSame($ignoreDummy, $highPriority->ignore());
+	}
+
+	public function testCanUseInto() {
+		$commandsBuilderMock = $this->getMock(\Pribi\Builders\CommandsBuilder::class);
+		$tableIdentifierDummy = $this->createIdentifierDummy();
+		$columnsIdentifierDummy = $this->createIdentifiersDummy();
+		/** @var \Pribi\Builders\CommandsBuilder $commandsBuilderMock */
+		$highPriority = $this->createHighPriority($commandsBuilderMock);
+		$createdStatementDummy = 'foo';
+		/** @var \PHPUnit_Framework_MockObject_MockObject $commandsBuilderMock */
+		$commandsBuilderMock
+			->expects($this->once())
+			->method('createInto')
+			->with($tableIdentifierDummy, $columnsIdentifierDummy, $highPriority)
+			->willReturn($createdStatementDummy);
+		$tableName = 'bar';
+		$commandsBuilderMock
+			->expects($this->once())
+			->method('createIdentifier')
+			->with($tableName)
+			->willReturn($tableIdentifierDummy);
+		$columnNames = ['baz', 'qux'];
+		$commandsBuilderMock
+			->expects($this->once())
+			->method('createIdentifiers')
+			->with($columnNames)
+			->willReturn($columnsIdentifierDummy);
+		$this->assertSame($createdStatementDummy, $highPriority->into($tableName, $columnNames));
+	}
+
+	private function createHighPriority(\Pribi\Builders\CommandsBuilder $commandsBuilder) {
+		return new HighPriority($this->createCommandDummy(), $commandsBuilder);
+	}
+}
+ 
