7dc66f76f124db5bbdd06e163f49cf0ed81755d7
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-08-16 12:16:50 +0200

    refactoring: Shortened code of DO test.

--- tests/Pribi/Commands/AnyQueryStatements/Executions/DoCommandTest.php
+++ tests/Pribi/Commands/AnyQueryStatements/Executions/DoCommandTest.php
@@ -4,17 +4,7 @@ namespace Pribi\Commands\AnyQueryStatements\Executions;
 class DoCommandTest extends \Tests\Helpers\CommandTestCase {
 
 	public function testCanCreateInstance() {
-		/** @var \Pribi\Commands\Identifiers\Identifier $identifier */
-		$identifier = $this->getMockBuilder(\Pribi\Commands\Identifiers\Identifier::class)
-			->disableOriginalConstructor()
-			->getMock();
-		/** @var \Pribi\Commands\Command $previousCommandMock */
-		$previousCommandMock = $this->getMockBuilder(\Pribi\Commands\Command::class)
-			->disableOriginalConstructor()
-			->getMockForAbstractClass();
-
-		$instance = new DoCommand($identifier, $previousCommandMock, $this->getCommandsBuilderDummy());
+		$instance = new DoCommand($this->createIdentifierDummy(), $this->createCommandDummy(), $this->getCommandsBuilderDummy());
 		$this->assertNotNull($instance);
 	}
 }
- 
