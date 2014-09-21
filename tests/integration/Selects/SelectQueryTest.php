b3eccaf205e3f6f293c42f95f9f20b194309f79d
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-09-21 19:47:21 +0200

    Select query composer from FROM and WHERE covered by test.

--- tests/integration/Selects/SelectQueryTest.php
+++ tests/integration/Selects/SelectQueryTest.php
@@ -42,4 +42,15 @@ class SelectQueryTest extends \PHPUnit_Framework_TestCase {
 				->execute()
 		);
 	}
+
+	public function testCanSelectFromTableWhereSomething() {
+		$this->assertSame(
+			'SELECT `foo` FROM `bar` WHERE `baz`',
+			$this->pribi->openQuery()
+				->select('foo')
+				->from('bar')
+				->where('baz')
+				->execute()
+		);
+	}
 }
