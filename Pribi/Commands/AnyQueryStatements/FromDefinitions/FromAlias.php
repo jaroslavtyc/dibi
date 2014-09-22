81451dcac7f77f96350cc192307f9d238a173611
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-09-22 08:45:57 +0200

    FROM alias defines possible next statements by itself, without a trait.

--- Pribi/Commands/AnyQueryStatements/FromDefinitions/FromAlias.php
+++ Pribi/Commands/AnyQueryStatements/FromDefinitions/FromAlias.php
@@ -1,14 +1,11 @@
 <?php
 namespace Pribi\Commands\AnyQueryStatements\FromDefinitions;
 
-class FromAlias extends \Pribi\Commands\Identifiers\IdentifierAlias implements \Pribi\Commands\AnyQueryStatements\FromDefinitions\Base\FromIdentifiable,
+class FromAlias extends \Pribi\Commands\Identifiers\IdentifierAlias implements
+	\Pribi\Commands\AnyQueryStatements\FromDefinitions\Base\FromIdentifiable,
 	\Pribi\Commands\AnyQueryStatements\WhereConditions\Parts\Whereable,
 	\Pribi\Commands\AnyQueryStatements\Joins\Parts\Joinable,
 	\Pribi\Commands\AnyQueryStatements\Limits\Base\Limitable {
-
-	use \Pribi\Commands\AnyQueryStatements\WhereConditions\Parts\Whereing;
-	use \Pribi\Commands\AnyQueryStatements\Joins\Joining;
-	use \Pribi\Commands\AnyQueryStatements\Limits\Base\Limiting;
 
 	public function __construct(
 		\Pribi\Commands\Identifiers\Identifier $alias,
@@ -20,5 +17,78 @@ class FromAlias extends \Pribi\Commands\Identifiers\IdentifierAlias implements \
 
 	protected function toSql() {
 		return 'AS ' . $this->getIdentifier()->toSql();
+	}
+
+	/**
+	 * @param string $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\Joins\InnerJoin
+	 */
+	public function innerJoin($subject) {
+		/** @var \Pribi\Commands\Command $this */
+		return $this->getCommandBuilder()->createAnyQueryInnerJoin(
+			$this->getCommandBuilder()->createIdentifier($subject),
+			$this
+		);
+	}
+
+	/**
+	 * @param $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\Joins\LeftJoin
+	 */
+	public function leftJoin($subject) {
+		return $this->getCommandBuilder()->createAnyQueryLeftJoin(
+			$this->getCommandBuilder()->createIdentifier($subject),
+			$this
+		);
+	}
+
+	/**
+	 * @param $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\Joins\RightJoin
+	 */
+	public function rightJoin($subject) {
+		return $this->getCommandBuilder()->createAnyQueryRightJoin(
+			$this->getCommandBuilder()->createIdentifier($subject),
+			$this
+		);
+	}
+
+	/**
+	 * @param string $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\WhereConditions\Where
+	 */
+	public function where($subject) {
+		return $this->getCommandBuilder()->createAnyQueryWhere(
+			$this->getCommandBuilder()->createIdentifier($subject),
+			$this
+		);
+	}
+
+	/**
+	 * @param string $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\WhereConditions\WhereNot
+	 */
+	public function whereNot($subject) {
+		return $this->getCommandBuilder()->createAnyQueryWhereNot(
+			$this->getCommandBuilder()->createIdentifier($subject),
+			$this
+		);
+	}
+
+	/**
+	 * @param int $limit
+	 * @return \Pribi\Commands\AnyQueryStatements\Limits\Limit
+	 */
+	public function limit($limit) {
+		return $this->getCommandBuilder()->createAnyQueryLimit(0, $limit, $this);
+	}
+
+	/**
+	 * @param int $offset
+	 * @param int $limit
+	 * @return \Pribi\Commands\AnyQueryStatements\Limits\Limit
+	 */
+	public function offsetAndLimit($offset, $limit) {
+		return $this->getCommandBuilder()->createAnyQueryLimit($offset, $limit, $this);
 	}
 }
