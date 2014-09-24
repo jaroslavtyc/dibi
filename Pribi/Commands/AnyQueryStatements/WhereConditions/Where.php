ab671e69cb5b1b356054f79881512ad1ba3ca765
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-09-24 18:56:49 +0200

    Removed trait with comparing methods from WHERE due to explicit definition of these methods in that class.

--- Pribi/Commands/AnyQueryStatements/WhereConditions/Where.php
+++ Pribi/Commands/AnyQueryStatements/WhereConditions/Where.php
@@ -12,7 +12,6 @@ class Where extends \Pribi\Commands\WithIdentifier implements
 	\Pribi\Commands\AnyQueryStatements\Conditions\Parts\Comparable {
 
 	use \Pribi\Commands\AnyQueryStatements\Conditions\Parts\AndOring;
-	use \Pribi\Commands\AnyQueryStatements\Conditions\Parts\Comparing;
 
 	protected function toSql() {
 		return 'WHERE ' . $this->getIdentifier()->toSql();
