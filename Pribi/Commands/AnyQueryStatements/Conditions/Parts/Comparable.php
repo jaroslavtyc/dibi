ec49e94a11a60ba075aa87aca581d45adb97ee1f
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-09-24 18:55:26 +0200

    Comparable interface has PHPdocumented expected return values.

--- Pribi/Commands/AnyQueryStatements/Conditions/Parts/Comparable.php
+++ Pribi/Commands/AnyQueryStatements/Conditions/Parts/Comparable.php
@@ -2,15 +2,40 @@
 namespace Pribi\Commands\AnyQueryStatements\Conditions\Parts;
 
 interface Comparable {
+
+	/**
+	 * @param $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\Conditions\EqualTo
+	 */
 	public function equalTo($subject);
 
+	/**
+	 * @param $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\Conditions\EqualOrGreaterThen
+	 */
 	public function equalOrGreaterThen($subject);
 
+	/**
+	 * @param $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\Conditions\EqualOrLesserThen
+	 */
 	public function equalOrLesserThen($subject);
 
+	/**
+	 * @param $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\Conditions\GreaterThen
+	 */
 	public function greaterThen($subject);
 
+	/**
+	 * @param $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\Conditions\LesserThen
+	 */
 	public function lesserThen($subject);
 
+	/**
+	 * @param $subject
+	 * @return \Pribi\Commands\AnyQueryStatements\Conditions\DifferentTo
+	 */
 	public function differentTo($subject);
 }
