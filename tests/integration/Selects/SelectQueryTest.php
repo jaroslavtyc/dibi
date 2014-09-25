ebf437b2c81f2aaaeb003cadb1fee61beb0d410b
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-09-25 18:06:44 +0200

    Integration tests for conditions after WHERE statement at a select query.

--- tests/integration/Selects/SelectQueryTest.php
+++ tests/integration/Selects/SelectQueryTest.php
@@ -65,4 +65,40 @@ class SelectQueryTest extends \PHPUnit_Framework_TestCase {
 				->execute()
 		);
 	}
+
+	public function testCanSelectFromTableWhereSomethingOrSomethingElse() {
+		$this->assertSame(
+			'SELECT `foo` FROM `bar` WHERE `baz` OR `qux`',
+			$this->pribi->openQuery()
+				->select('foo')
+				->from('bar')
+				->where('baz')
+				->or('qux')
+				->execute()
+		);
+	}
+
+	public function testCanSelectFromTableWhereSomethingAndNotSomethingElse() {
+		$this->assertSame(
+			'SELECT `foo` FROM `bar` WHERE `baz` AND NOT `qux`',
+			$this->pribi->openQuery()
+				->select('foo')
+				->from('bar')
+				->where('baz')
+				->andNot('qux')
+				->execute()
+		);
+	}
+
+	public function testCanSelectFromTableWhereSomethingOrNotSomethingElse() {
+		$this->assertSame(
+			'SELECT `foo` FROM `bar` WHERE `baz` OR NOT `qux`',
+			$this->pribi->openQuery()
+				->select('foo')
+				->from('bar')
+				->where('baz')
+				->orNot('qux')
+				->execute()
+		);
+	}
 }
