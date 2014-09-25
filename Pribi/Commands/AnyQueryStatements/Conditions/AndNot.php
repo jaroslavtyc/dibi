c5382f4660acdbbf39410db498048fbc71ddb54f
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-09-25 18:04:02 +0200

    refactoring: New lines added between class descriptors.

--- Pribi/Commands/AnyQueryStatements/Conditions/AndNot.php
+++ Pribi/Commands/AnyQueryStatements/Conditions/AndNot.php
@@ -2,7 +2,9 @@
 namespace Pribi\Commands\AnyQueryStatements\Conditions;
 
 class AndNot extends Conjunction {
+
 	protected function toSql() {
 		return 'AND NOT ' . $this->getIdentifier()->toSql();
 	}
+
 }
--- Pribi/Commands/AnyQueryStatements/Conditions/OrNot.php
+++ Pribi/Commands/AnyQueryStatements/Conditions/OrNot.php
@@ -1,10 +1,10 @@
 <?php
 namespace Pribi\Commands\AnyQueryStatements\Conditions;
 
-use Pribi\Commands\AnyQueryStatements\Conditions\Parts\Disjunction;
-
 class OrNot extends Disjunction {
+
 	protected function toSql() {
 		return 'OR NOT ' . $this->getIdentifier()->toSql();
 	}
+
 }
