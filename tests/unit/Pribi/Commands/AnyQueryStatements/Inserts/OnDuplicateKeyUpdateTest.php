bbbcd8862f69a534659f33bd6d941b9faf763a42
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-10-04 01:47:24 +0200

    fix ON DUPLICATE KEY UPDATE is tested for chan of same statement

--- tests/unit/Pribi/Commands/AnyQueryStatements/Inserts/OnDuplicateKeyUpdateTest.php
+++ tests/unit/Pribi/Commands/AnyQueryStatements/Inserts/OnDuplicateKeyUpdateTest.php
@@ -60,4 +60,28 @@ class OnDuplicateKeyUpdateTest extends \tests\unit\helpers\CommandTestCase {
 		);
 		$this->assertSame(",$identifierAsSql=$expressionAsSql", $toSqlMethod->invoke($onDuplicateKeyUpdate));
 	}
+
+	public function testCanBeFollowedByOnDuplicateKeyUpdate() {
+		$columnName = 'foo';
+		$expression = 'bar';
+		$columnIdentifierDummy = $this->createIdentifierDummy();
+		$expressionSubjectDummy = $this->createSubjectDummy();
+		$onDuplicateKeyUpdateDummy = 'foobaz';
+		$commandBuilderMock = $this->createCommandBuilderMock();
+		$commandBuilderMock->expects($this->once())
+			->method('createOnDuplicateKeyUpdate')
+			->with($columnIdentifierDummy, $expressionSubjectDummy)
+			->willReturn($onDuplicateKeyUpdateDummy);
+		$commandBuilderMock->expects($this->once())
+			->method('createIdentifier')
+			->with($columnName)
+			->willReturn($columnIdentifierDummy);
+		$commandBuilderMock->expects($this->once())
+			->method('createSubject')
+			->with($expression)
+			->willReturn($expressionSubjectDummy);
+		/** @var \Pribi\Builders\Commands\Builder $commandBuilderMock */
+		$onDuplicateKeyUpdate = new OnDuplicateKeyUpdate($this->createIdentifierDummy(), $this->createSubjectDummy(), $this->createCommandDummy(), $commandBuilderMock);
+		$this->assertSame($onDuplicateKeyUpdateDummy, $onDuplicateKeyUpdate->onDuplicateKeyUpdate($columnName, $expression));
+	}
 }
