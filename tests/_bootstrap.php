1be0466959de1e21d923dc30f5e627cfa644b5ee
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-10-06 21:56:13 +0200

    Pribi test autoloader comment moved closer to described functionality

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,14 +1,12 @@
 <?php
 // This is global bootstrap for autoloading
 
+require __DIR__ . '/../vendor/autoload.php';
+
 /*
  * Simple autoloader using the PCR-0 Pribi naming convention, @see http://www.php-fig.org/psr/psr-0/
  * For example Pribi\Commands\AnyQueryStatements\Inserts\Insert is searched in "__DIR__ . '/../Pribi/Commands/Inserts/Insert.php" file.
  */
-
-require __DIR__ . '/../vendor/autoload.php';
-
-// Pribi lazy autoload
 spl_autoload_register(function ($className) {
 	$classFilename = __DIR__ . '/../' . str_replace('\\', '/', $className) . '.php';
 	if (file_exists($classFilename)) {
