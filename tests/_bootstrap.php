83a3d8b7f3d5821e756a38363dc1947073e2bda1
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-10-08 21:04:53 +0200

    refactoring: Registering autoloader for tests has explicitly listed settings.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -7,9 +7,13 @@ require __DIR__ . '/../vendor/autoload.php';
  * Simple autoloader using the PCR-0 Pribi naming convention, @see http://www.php-fig.org/psr/psr-0/
  * For example Pribi\Commands\AnyQueryStatements\Inserts\Insert is searched in "__DIR__ . '/../Pribi/Commands/Inserts/Insert.php" file.
  */
-spl_autoload_register(function ($className) {
-	$classFilename = __DIR__ . '/../' . str_replace('\\', '/', $className) . '.php';
-	if (file_exists($classFilename)) {
-		require $classFilename;
-	}
-});
+spl_autoload_register(
+	function ($className) {
+		$classFilename = __DIR__ . '/../' . str_replace('\\', '/', $className) . '.php';
+		if (file_exists($classFilename)) {
+			require $classFilename;
+		}
+	},
+	TRUE /* throw exception if this autoloader can not be registered */,
+	FALSE /* do not prepend this autoloader before already registered */
+);
