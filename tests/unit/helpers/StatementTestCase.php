33ca75607ad1379224bcfc1f0389403d38539507
Jaroslav Tyc <jaroslavtyc@atlas.cz>
2014-10-08 21:05:54 +0200

    refactoring: Code wrapped.

--- tests/unit/helpers/StatementTestCase.php
+++ tests/unit/helpers/StatementTestCase.php
@@ -4,7 +4,10 @@ namespace tests\unit\helpers;
 class StatementTestCase extends CommandTestCase {
 
 	protected function huntUnexpectedFollowingStatements(){
-		$hunter = new \tests\unit\helpers\HunterOfUnexpectedFollowingStatement(new AvailableTestsFinder, new AvailableStatementsFinder());
+		$hunter = new \tests\unit\helpers\HunterOfUnexpectedFollowingStatement(
+			new AvailableTestsFinder,
+			new AvailableStatementsFinder()
+		);
 		$hunter->hunt(preg_replace('~Test$~', '', static::class));
 	}
 
